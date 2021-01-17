97348f7a17e5015dfa916adb4e970c882a2da534
Amy Hua <aymay98@gmail.com>
2021-01-16 23:24:19 -0600

    polymorphismCar initialized

---
+++ polymorphismCar/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    private boolean engine;
+        private int cylinders;
+	    private String name;
+
+    }
+}
