7ea74167a4772817ac9b4b0b07e585ef4e3ae8e7
Amy Hua <aymay98@gmail.com>
2021-01-24 10:45:36 -0600

    Sorted array initalized

--- arrays/src/com/company/Main.java
+++ arrays/src/com/company/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
 
     private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         int[] myIntegers = getIntegers(5);
         for(int i = 0; i < myIntegers.length; i++){
