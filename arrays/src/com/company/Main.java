8178a2d7d36726f1069092f0078a4498bf8b5a02
Amy Hua <aymay98@gmail.com>
2021-01-24 10:11:03 -0600

    Array example

--- arrays/src/com/company/Main.java
+++ arrays/src/com/company/Main.java
@@ -1,8 +1,40 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
+    private static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-	    int[] myVariables = new int[10];
+        int[] myIntegers = getIntegers(5);
+        for(int i = 0; i < myIntegers.length; i++){
+            System.out.println("Element " + i + ", typed value was " + myIntegers[i]);
+        }
+        System.out.println("The average is " + getAverage(myIntegers));
+
+//	    int[] myIntArray = new int[10];
+//	    myIntArray[5] = 50;
+//        double[] myDoubleArray = new double[10];
+//        System.out.println(myIntArray[5]);
+
+    }
+
+    public static int[] getIntegers(int number){
+        System.out.println("Enter " + number + " integer values. \r");
+        int[] values = new int[number];
+
+        for (int i = 0; i < values.length; i++){
+            values[i] = scanner.nextInt();
+        }
+
+        return values;
+    }
+
+    public static double getAverage (int[] values){
+        int sum = 0;
+        for (int i = 0; i < values.length; i++){
+            sum += values[i];
+        }
+        return (double) sum / (double)values.length;
     }
 }
