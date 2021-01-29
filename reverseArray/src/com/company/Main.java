3ad67eaa61b39f6436be8eb9f503232203f42bc7
Amy Hua <aymay98@gmail.com>
2021-01-29 13:20:04 -0600

    reverse

--- reverseArray/src/com/company/Main.java
+++ reverseArray/src/com/company/Main.java
@@ -1,12 +1,15 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 
     }
 
-    private static void reverse(){
+    private static void reverse(int[] array){
+        System.out.println("Array = " + Arrays.toString(array));
 
     }
 }
