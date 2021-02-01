726d1b0d8ad520c9cec6588e596642ec2ba9081d
Amy Hua <aymay98@gmail.com>
2021-01-31 23:19:55 -0600

    reverse() completed.

--- reverseArray/src/com/company/Main.java
+++ reverseArray/src/com/company/Main.java
@@ -5,11 +5,22 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-
+        int[] array = {1,2,3,4,5};
+        reverse(array);
     }
 
     private static void reverse(int[] array){
-        System.out.println("Array = " + Arrays.toString(array));
+        int first = 0;
+        int last = array.length-1;
 
+        System.out.println("Array = " + Arrays.toString(array));
+        while(last>first){
+            int temp = array[first];
+            array[first] = array[last];
+            array[last] = temp;
+            first++;
+            last--;
+        }
+        System.out.println("Reversed array = " + Arrays.toString(array));
     }
 }
