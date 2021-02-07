ae224828e5976be8726a5b8b71c163edeb021c9d
Amy Hua <aymay98@gmail.com>
2021-02-06 21:40:43 -0600

    Lecture 109 completed.

--- section 8 - arrays, lists, autobox, unbox/mobilePhone/src/com/company/MobilePhone.java
+++ section 8 - arrays, lists, autobox, unbox/mobilePhone/src/com/company/MobilePhone.java
@@ -31,11 +31,22 @@ public class MobilePhone {
         return true;
     }
 
+    public boolean removeContact(Contact contact){
+        int foundPosition = findContact(contact);
+        if(foundPosition < 0){
+            System.out.println(contact.getName() + " was not found.");
+            return false;
+        }
+        this.myContacts.remove(foundPosition);
+        System.out.println(contact.getName() + " was deleted.");
+        return true;
+    }
+
     private int findContact(Contact contact){
         return this.myContacts.indexOf(contact);
     }
 
-    public int findContact(String contactName){
+    private int findContact(String contactName){
         for (int i = 0; i<this.myContacts.size(); i++){
             Contact contact = this.myContacts.get(i);
             if (contact.getName().equals(contactName)){
@@ -45,5 +56,28 @@ public class MobilePhone {
         return -1;
     }
 
+    public String queryContact(Contact contact){
+        if(findContact(contact)>0){
+            return contact.getName();
+        }
+        return null;
+    }
+
+    public Contact queryContact(String name){
+        int position = findContact(name);
+        if(position >= 0){
+            return this.myContacts.get(position);
+        }
+        return null;
+    }
+
+    public void printContacts(){
+        System.out.println("Contact List");
+        for (int i = 0; i < myContacts.size(); i++){
+            System.out.println((i + 1) + ". " +
+                    this.myContacts.get(i).getName() + " -> " +
+                    this.myContacts.get(i).getPhoneNumber());
+        }
+    }
 
 }
