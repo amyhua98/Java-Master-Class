b94c06567690e43ae63a33bc9c986360b8ef4aa3
Amy Hua <aymay98@gmail.com>
2021-02-09 20:20:34 -0600

    mobilePhone commit.

--- section 8 - arrays, lists, autobox, unbox/mobilePhone/src/com/company/Main.java
+++ section 8 - arrays, lists, autobox, unbox/mobilePhone/src/com/company/Main.java
@@ -76,6 +76,32 @@ public class Main {
         }
     }
 
+    private static void removeContact(){
+        System.out.println("Enter existing contact name: ");
+        String name = scanner.nextLine();
+        Contact existingContactRecord = mobilePhone.queryContact(name);
+        if (existingContactRecord == null){
+            System.out.println("Contact was not found.");
+            return;
+        }
+        if(mobilePhone.removeContact(existingContactRecord)){
+            System.out.println("Successfully deleted.");
+        }else{
+            System.out.println("Error deleting contact.");
+        }
+    }
+
+    private static void queryContact(){
+        System.out.println("Enter existing contact name: ");
+        String name = scanner.nextLine();
+        Contact existingContactRecord = mobilePhone.queryContact(name);
+        if (existingContactRecord == null){
+            System.out.println("Contact was not found.");
+            return;
+        }
+        System.out.println("Name: " + existingContactRecord.getName() + " phone number is " + existingContactRecord.getPhoneNumber());
+    }
+
 
     private static void startPhone(){
         System.out.println("Starting phone ...");
