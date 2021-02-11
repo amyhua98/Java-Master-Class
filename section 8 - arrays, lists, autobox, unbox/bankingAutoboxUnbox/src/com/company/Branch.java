f707d3de169fb468fe533494f1840fa568c86143
Amy Hua <aymay98@gmail.com>
2021-02-11 11:57:47 -0600

    banking completed.

---
+++ section 8 - arrays, lists, autobox, unbox/bankingAutoboxUnbox/src/com/company/Branch.java
@@ -0,0 +1,50 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Branch {
+        private String name;
+        private ArrayList<Customer> customers;
+
+    public Branch(String name) {
+        this.name = name;
+        this.customers =  new ArrayList<Customer>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Customer> getCustomers() {
+        return customers;
+    }
+
+    public boolean newCustomer(String name, double initalAmount){
+        if(findCustomer(name) == null){
+            this.customers.add(new Customer(name,initalAmount));
+            return true;
+        }
+        return false;
+    }
+
+    public boolean addCustomerTransaction(String name, double amount){
+        Customer existingCustomer = findCustomer(name);
+        if(existingCustomer != null){
+            existingCustomer.addTransaction(amount);
+            return true;
+        }
+        return false;
+    }
+
+    public Customer findCustomer(String name){
+        for (int i = 0; i < customers.size(); i++){
+            Customer checkedCustomer = customers.get(i);
+            if(checkedCustomer.getName().equals(name)){
+                return checkedCustomer;
+            }
+        }
+        return null;
+    }
+
+
+}
--- section 8 - arrays, lists, autobox, unbox/bankingAutoboxUnbox/src/com/company/Main.java
+++ section 8 - arrays, lists, autobox, unbox/bankingAutoboxUnbox/src/com/company/Main.java
@@ -3,6 +3,42 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Bank bank = new Bank("Bank of America");
+
+        if (bank.addBranch("Chicago")) {
+            System.out.println("Chicago branch created");
+        }
+
+        bank.addCustomer("Chicago", "Tim", 259.45);
+        bank.addCustomer("Chicago", "Kevin", 185.31);
+        bank.addCustomer("Chicago", "Robert", 225.62);
+
+        bank.addBranch("California");
+        bank.addCustomer("California", "Bob", 190.59);
+
+        bank.addCustomerTransaction("California", "Bob", 50.26);
+        bank.addCustomerTransaction("California", "Bob", 67.23);
+        bank.addCustomerTransaction("California", "John", 45.15);
+
+        bank.listCustomers("Chicago", true);
+        bank.listCustomers("California", true);
+
+        bank.addBranch("Texas");
+
+        if (!bank.addCustomer("Texas", "Donald", 5.53)) {
+            System.out.println("Error Texas branch does not exist");
+        }
+
+        if (!bank.addBranch("Chicago")) {
+            System.out.println("New York branch already exists");
+        }
+
+        if (!bank.addCustomerTransaction("Chicago", "Smith", 120.69)) {
+            System.out.println("Customer does not exist at branch");
+        }
+
+        if (!bank.addCustomer("Chicago", "John", 36.20)) {
+            System.out.println("Customer Tim already exists");
+        }
     }
 }
