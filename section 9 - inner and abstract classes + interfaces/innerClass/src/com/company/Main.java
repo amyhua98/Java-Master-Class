7787f51066c406ba3df21741202142c346436511
Amy Hua <aymay98@gmail.com>
2021-03-01 13:51:58 -0600

    innerclass updated.

--- section 9 - inner and abstract classes + interfaces/innerClass/src/com/company/Main.java
+++ section 9 - inner and abstract classes + interfaces/innerClass/src/com/company/Main.java
@@ -1,8 +1,27 @@
 package com.company;
 
-public class Main {
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
-    public static void main(String[] args) {
-	// write your code here
+
+class Main {
+    public static void main(String[] args) throws InterruptedException {
+
+        class MyClass {
+            public void printMe(Object o) {
+                System.out.println("Hello "+o);
+            }
+
+            public void printMe(String o) {
+                System.out.println(100+o);
+            }
+
+            public void printMe(Integer o) {
+                System.out.println(o);
+            }
+        }
+        MyClass my = new MyClass();
+        my.printMe("Java");
     }
 }
