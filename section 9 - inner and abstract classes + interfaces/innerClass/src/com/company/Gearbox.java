e90e6d96a3d2340caed4c3ae941fbad9c7bdc712
Amy Hua <aymay98@gmail.com>
2021-03-03 14:39:05 -0600

    Update Main

--- section 9 - inner and abstract classes + interfaces/innerClass/src/com/company/Gearbox.java
+++ section 9 - inner and abstract classes + interfaces/innerClass/src/com/company/Gearbox.java
@@ -6,12 +6,17 @@ public class Gearbox {
     private ArrayList<Gear> gears;
     private int maxGears;
     private int currentGear = 0;
+    private boolean clutchIsIn;
 
     public Gearbox(int maxGears) {
         this.maxGears = maxGears;
         this.gears = new ArrayList<>();
         Gear neutral = new Gear(0, 0.0);
         this.gears.add(neutral);
+    }
+
+    public void operateClutch(boolean in){
+        this.cl
     }
 
     public class Gear{
