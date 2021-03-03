92832bf5ef9df2d9b843e0710f72aace19064ea5
Amy Hua <aymay98@gmail.com>
2021-03-03 11:44:53 -0600

    Update Main

--- section 9 - inner and abstract classes + interfaces/innerClass/src/com/company/Main.java
+++ section 9 - inner and abstract classes + interfaces/innerClass/src/com/company/Main.java
@@ -5,6 +5,12 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-
+        Gearbox mcLaren = new Gearbox(6);
+        Gearbox.Gear first = mcLaren.new Gear(1,12.3);
+//        error bc Gearbox was not created first
+//        Gearbox.Gear second = new Gearbox.Gear(2,15.4);
+//        error bc you format is incorrect
+//        Gearbox.Gear third = new mcLaren.Gear(3,17.3);
+        System.out.println(first.driveSpeed(1000));
     }
 }
