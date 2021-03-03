44e4218702099b7a0b6fe5249644cc945a5e800f
Amy Hua <aymay98@gmail.com>
2021-03-02 22:42:25 -0600

    Made Gear inner class

--- section 9 - inner and abstract classes + interfaces/innerClass/src/com/company/Gearbox.java
+++ section 9 - inner and abstract classes + interfaces/innerClass/src/com/company/Gearbox.java
@@ -1,5 +1,30 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Gearbox {
-    //fd
+    private ArrayList<Gear> gears;
+    private int maxGears;
+    private int currentGear = 0;
+
+    public Gearbox(int maxGears) {
+        this.maxGears = maxGears;
+        this.gears = new ArrayList<>();
+        Gear neutral = new Gear(0, 0.0);
+        this.gears.add(neutral);
+    }
+
+    public class Gear{
+        private int gearNumber;
+        private double ratio;
+
+        public Gear(int gearNumber, double ratio) {
+            this.gearNumber = gearNumber;
+            this.ratio = ratio;
+        }
+
+        public double driveSpeed(int revs){
+            return revs * (this.ratio);
+        }
+    }
 }
--- section 9 - inner and abstract classes + interfaces/innerClass/src/com/company/Main.java
+++ section 9 - inner and abstract classes + interfaces/innerClass/src/com/company/Main.java
@@ -1,27 +1,10 @@
 package com.company;
 
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
 
+public class Main {
 
-class Main {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
 
-        class MyClass {
-            public void printMe(Object o) {
-                System.out.println("Hello "+o);
-            }
-
-            public void printMe(String o) {
-                System.out.println(100+o);
-            }
-
-            public void printMe(Integer o) {
-                System.out.println(o);
-            }
-        }
-        MyClass my = new MyClass();
-        my.printMe("Java");
     }
 }
