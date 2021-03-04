8def82d95e8573413bb3a753da5ba79719fdba71
Amy Hua <aymay98@gmail.com>
2021-03-03 23:45:24 -0600

    innerClass finished.

--- section 9 - inner and abstract classes + interfaces/innerClass/src/com/company/Gearbox.java
+++ section 9 - inner and abstract classes + interfaces/innerClass/src/com/company/Gearbox.java
@@ -13,10 +13,35 @@ public class Gearbox {
         this.gears = new ArrayList<>();
         Gear neutral = new Gear(0, 0.0);
         this.gears.add(neutral);
+
     }
 
     public void operateClutch(boolean in){
         this.clutchIsIn = in;
+    }
+
+    public void addGear(int number, double ratio){
+        if ((number > 0) && (number <= maxGears)){
+            this.gears.add(new Gear(number, ratio));
+        }
+    }
+
+    public void changeGear(int newGear){
+        if((newGear>= 0) && (newGear < this.gears.size() && this.clutchIsIn)){
+            this.currentGear = newGear;
+            System.out.println("Gear " + newGear + " selected.");
+        }else{
+            System.out.println("Grind!");
+            this.currentGear = 0;
+        }
+    }
+
+    public double wheelSpeed(int revs){
+        if (clutchIsIn){
+            System.out.println("Scream!!!");
+            return 0.0;
+        }
+        return revs * gears.get(currentGear).getRatio();
     }
 
     public class Gear{
@@ -26,6 +51,10 @@ public class Gearbox {
         public Gear(int gearNumber, double ratio) {
             this.gearNumber = gearNumber;
             this.ratio = ratio;
+        }
+
+        public double getRatio() {
+            return ratio;
         }
 
         public double driveSpeed(int revs){
--- section 9 - inner and abstract classes + interfaces/innerClass/src/com/company/Main.java
+++ section 9 - inner and abstract classes + interfaces/innerClass/src/com/company/Main.java
@@ -1,16 +1,63 @@
 package com.company;
 
+import java.sql.SQLOutput;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+    private static Button btnPrint = new Button("Print");
 
     public static void main(String[] args) {
-        Gearbox mcLaren = new Gearbox(6);
-        Gearbox.Gear first = mcLaren.new Gear(1,12.3);
-//        error bc Gearbox was not created first
-//        Gearbox.Gear second = new Gearbox.Gear(2,15.4);
-//        error bc you format is incorrect
-//        Gearbox.Gear third = new mcLaren.Gear(3,17.3);
-        System.out.println(first.driveSpeed(1000));
+
+//        class ClickListener implements Button.OnClickListener{
+//            public ClickListener(){
+//                System.out.println("I've been attached");
+//            }
+//
+//            @Override
+//            public void onClick(String title) {
+//                System.out.println(title + " was clicked");
+//            }
+//        }
+//        btnPrint.setOnClickedListener(new ClickListener());
+
+        btnPrint.setOnClickedListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(String title) {
+                System.out.println(title + " was clicked");
+            }
+        });
+
+//        Gearbox mcLaren = new Gearbox(6);
+//        mcLaren.addGear(1,5.3);
+//        mcLaren.addGear(2,10.6);
+//        mcLaren.addGear(3,15.9);
+//        mcLaren.operateClutch(true);
+//        mcLaren.changeGear(1);
+//        mcLaren.operateClutch(false);
+//        System.out.println(mcLaren.wheelSpeed(1000));
+//        mcLaren.changeGear(2);
+//        System.out.println(mcLaren.wheelSpeed(3000));
+//        mcLaren.operateClutch(true);
+//        mcLaren.changeGear(3);
+//        mcLaren.operateClutch(false);
+//        System.out.println(mcLaren.wheelSpeed(6000));
+
+    }
+
+    private static void listen(){
+        boolean quit = false;
+        while(!quit){
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            switch(choice){
+                case 0:
+                    quit = true;
+                    break;
+                case 1:
+                    btnPrint.onClick();
+            }
+        }
     }
 }
