306b7a59e863c1183d068e5770e9317c2ff166af
Amy Hua <aymay98@gmail.com>
2021-03-16 00:10:45 -0500

    abstractChallenge update.

---
+++ section 9 - inner and abstract classes + interfaces/abstractChallenge/src/com/company/MyLinkedList.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class MyLinkedList implements NodeList{
+
+    ListItem root;
+
+    public MyLinkedList(ListItem root) {
+        this.root = root;
+    }
+
+    @Override
+    public ListItem getRoot() {
+        return root;
+    }
+
+    @Override
+    public boolean addItem(ListItem item) {
+        if ()
+        return true;
+    }
+
+    @Override
+    public boolean removeItem(ListItem item) {
+        return true;
+    }
+
+    @Override
+    public void traverse(ListItem item) {
+
+    }
+}
---
+++ section 9 - inner and abstract classes + interfaces/abstractChallenge/src/com/company/NodeList.java
@@ -0,0 +1,11 @@
+package com.company;
+
+interface NodeList {
+
+    ListItem getRoot();
+
+    boolean addItem(ListItem item);
+    boolean removeItem(ListItem item);
+    void traverse(ListItem item);
+
+}
