302216175b90ea9c7e513fa4efd7012d0fb367ea
Amy Hua <aymay98@gmail.com>
2021-03-18 22:36:35 -0500

    Testing on abstractClass

--- section 9 - inner and abstract classes + interfaces/abstractChallenge/src/com/company/MyLinkedList.java
+++ section 9 - inner and abstract classes + interfaces/abstractChallenge/src/com/company/MyLinkedList.java
@@ -14,9 +14,44 @@ public class MyLinkedList implements NodeList{
     }
 
     @Override
-    public boolean addItem(ListItem item) {
-        if ()
-        return true;
+    public boolean addItem(ListItem newItem) {
+        if (this.root == null){
+            //list is empty
+            this.root = newItem;
+            return true;
+        }
+        ListItem currentItem = this.root;
+        while(currentItem != null){
+            int comparison = (currentItem.compareTo(newItem));
+            if(comparison < 0){
+                //newItem is greater, move right if possible
+                if (currentItem.next() != null){
+                    currentItem = currentItem.next();
+                }else{
+                    //no next, so insert end of list
+                    currentItem.setNext(newItem);
+                    newItem.setPrevious(currentItem);
+                    return true;
+                }
+            }else if (comparison > 0){
+                // newItem is less, insert before
+                if (currentItem.previous() != null){
+                    currentItem.previous().setNext(newItem);
+                    newItem.setPrevious(currentItem.previous());
+                    newItem.setNext(currentItem);
+                    currentItem.setPrevious(newItem);
+                }else{
+                    newItem.setNext(this.root);
+                    this.root.setPrevious(newItem);
+                    this.root = newItem;
+                }
+                return true;
+            }else{
+                System.out.println(newItem.getValue() + " is already present, not added.");
+                return false;
+            }
+        }
+        return false;
     }
 
     @Override
--- section 9 - inner and abstract classes + interfaces/abstractClass/src/com/company/Main.java
+++ section 9 - inner and abstract classes + interfaces/abstractClass/src/com/company/Main.java
@@ -17,5 +17,6 @@ public class Main {
 
         Penguin penguin = new Penguin("Emperor");
         penguin.fly();
+
     }
 }
